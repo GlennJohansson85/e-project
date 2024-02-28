168432f48bb41b336370766795e7d645fb91909a
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-28 11:42:13 +0100

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -13,12 +12,12 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None                                                             # Set size equal to none initially
-    if 'product_size' in request.POST:                         # If product size in request.post 
-        size = request.POST['size']                                # well set it equal to that
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if size:                                                                                        # Check if a product with sizes is being added
+    if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
@@ -34,3 +33,52 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:                                                                                    # If quantity is greater than 0
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:                                                                            # 'll wrap this entire block of code in a try block. And catch any exceptions that happen in order to return a 500 server error.
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return redirect(reverse('view_bag'))                 # Because this view will be posted to from a JavaScript function. We want to return an actual 200 HTTP response. Implying that the item was successfully removed
+    except Exception as e:
+        return HttpResponse(status=500)                     # In a future video we'll use this variable e to return the actual error message to the template in case anything goes wrong.
