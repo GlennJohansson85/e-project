32c813e66087d45587a6679f62d6c1f856f63699
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-03-15 22:51:24 +0100

    Update settings.py

--- boutique/settings.py
+++ boutique/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
+from django.conf.global_settings import DEFAULT_AUTO_FIELD
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
